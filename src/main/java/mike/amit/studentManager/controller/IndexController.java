eaa886cfbc0eda2d3ac4cd9092897f0ffe7d1580
Michael Haile <MichaelHaile83@gmail.com>
2020-07-02 23:49:15 -0400

    Index Model attribute Created

--- src/main/java/mike/amit/studentManager/controller/IndexController.java
+++ src/main/java/mike/amit/studentManager/controller/IndexController.java
@@ -1,13 +1,19 @@
 package mike.amit.studentManager.controller;
 
+import mike.amit.studentManager.entity.Student;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-
 public class IndexController {
-    @RequestMapping("/")
-    public String index(){
+    @GetMapping("/")
+    public String getIndex(Model model){
+        model.addAttribute("student", new Student(1,
+                "Michael",
+                "Haile",
+                "email"));
         return "index";
     }
+
 }
---
+++ src/main/java/mike/amit/studentManager/repository/StudentRepository.java
@@ -0,0 +1,7 @@
+package mike.amit.studentManager.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StudentRepository {
+}
