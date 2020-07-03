f565cac00e0c54fb509db0e955ac3c0a93484093
Michael Haile <MichaelHaile83@gmail.com>
2020-07-03 03:18:24 -0400

    Pages Mapped in controller

--- src/main/java/mike/amit/studentManager/controller/IndexController.java
+++ src/main/java/mike/amit/studentManager/controller/IndexController.java
@@ -4,16 +4,32 @@ import mike.amit.studentManager.entity.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class IndexController {
-    @GetMapping("/")
-    public String getIndex(Model model){
-        model.addAttribute("student", new Student(1,
-                "Michael",
-                "Haile",
-                "email"));
-        return "index";
+    Student student;
+    //AddStudent Mapping
+    @GetMapping("addStudent")
+    public String addStudent(){
+        return "addStudent";
+    }
+
+    @PostMapping("add")
+    public String addStudent(Student student, Model model){
+        this.student = student;
+        if(this.student != null)model.addAttribute("message", "Success");
+        else model.addAttribute("message", "Failed");
+        return "addStudent";
+    }
+
+    @GetMapping("updatedList")
+    public String getUpdatedList(Model model){
+        model.addAttribute(student);
+        return "studentList";
     }
 
 }
--- src/main/java/mike/amit/studentManager/entity/Student.java
+++ src/main/java/mike/amit/studentManager/entity/Student.java
@@ -6,8 +6,10 @@ public class Student {
     private String lastName;
     private String email;
 
+    public Student() {
+    }
+
     public Student(long id, String firstName, String lastName, String email) {
-        this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
