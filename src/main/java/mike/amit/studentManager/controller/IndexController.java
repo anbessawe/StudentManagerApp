3549b59e00c9ef4151c9625525feacf9f20d7dcf
Michael Haile <MichaelHaile83@gmail.com>
2020-07-03 09:45:11 -0400

    Model Binding

--- src/main/java/mike/amit/studentManager/controller/IndexController.java
+++ src/main/java/mike/amit/studentManager/controller/IndexController.java
@@ -3,10 +3,13 @@ package mike.amit.studentManager.controller;
 import mike.amit.studentManager.entity.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/")
@@ -14,21 +17,22 @@ public class IndexController {
     Student student;
     //AddStudent Mapping
     @GetMapping("addStudent")
-    public String addStudent(){
+    public String showAddStudentPage(@ModelAttribute Student student, BindingResult result){
+        this.student = student;
         return "addStudent";
     }
 
-    @PostMapping("add")
-    public String addStudent(Student student, Model model){
+    @PostMapping("addStudent")
+    public String addStudentToList(@ModelAttribute Student student, BindingResult result, Model model){
         this.student = student;
-        if(this.student != null)model.addAttribute("message", "Success");
-        else model.addAttribute("message", "Failed");
+        if(result.hasErrors())model.addAttribute("message", "Failed");
+        else model.addAttribute("message", "Success");
         return "addStudent";
     }
 
     @GetMapping("updatedList")
     public String getUpdatedList(Model model){
-        model.addAttribute(student);
+        model.addAttribute("student",this.student);
         return "studentList";
     }
 
